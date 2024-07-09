19640b7e3aed4f92e765e4145e3dd9899c44d617
Elias Celis <140775870+zelys@users.noreply.github.com>
2024-07-09 09:10:48 -0400

    Update.java

--- src/main/java/com/aluracursos/screenmatch/principal/Principal.java
+++ src/main/java/com/aluracursos/screenmatch/principal/Principal.java
@@ -15,7 +15,7 @@ public class Principal {
     private Scanner teclado = new Scanner(System.in);
     private ConsumoAPI consumoApi = new ConsumoAPI();
     private final String URL_BASE = "https://www.omdbapi.com/?t=";
-    private final String API_KEY = "&apikey=c3437505";
+    private final String API_KEY = "&apikey=c343";
     private ConvierteDatos conversor = new ConvierteDatos();
     private List<DatosSerie> datosSeries = new ArrayList<>();
     private SerieRepository repositorio;
